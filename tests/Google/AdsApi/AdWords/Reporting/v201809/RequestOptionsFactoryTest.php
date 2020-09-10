e6cd8a8457a7c78239bc17cce970fe6c54c93149
mikeaag <mike.gatward@xenonsoft.co.uk>
2020-09-10 10:11:08 +0100

    Add support for guzzle v7 #679

--- tests/Google/AdsApi/AdWords/Reporting/v201809/RequestOptionsFactoryTest.php
+++ tests/Google/AdsApi/AdWords/Reporting/v201809/RequestOptionsFactoryTest.php
@@ -418,7 +418,7 @@ class RequestOptionsFactoryTest extends TestCase
 
     private function formatGuzzleInfo()
     {
-        $guzzleInfoTokens = ['GuzzleHttp/' . ClientInterface::VERSION];
+        $guzzleInfoTokens = ['GuzzleHttp/' . ClientInterface::MAJOR_VERSION];
         if (extension_loaded('curl') && function_exists('curl_version')) {
             $guzzleInfoTokens[] = 'curl/' . \curl_version()['version'];
         }
